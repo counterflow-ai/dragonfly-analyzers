20fb1ea682831c77a0b3a9081d32b959de8d83ed
Andrew Fast <af@counterflowai.com>
2019-04-16 15:17:38 -0400

    initial install script using rsync

---
+++ analyzer-install.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+dragonfly_root="/usr/local/dragonfly"
+destination="analyzer"
+analyzer_dirs="ip-util util"
+dir=$1
+
+if [ ! -d $dir ] &&  [ ! $dir = "all" ]; then 
+    echo "No such directory $dir"
+    exit
+fi
+
+analyzer_dirs="$analyzer_dirs $dir"
+
+if [ $dir = "all" ]; then
+    analyzer_dirs="anomaly event-triage ip-util machine-learning stats top-talkers util"
+elif [ $dir = "filter" ]; then
+    analyzer_dirs="filter"
+    destination="filter"
+elif [ $dir = "util" ]; then
+    analyzer_dirs="util"
+elif [ $dir = "ip-util" ]; then
+    analyzer_dirs="ip-util util"
+fi
+
+rsync_dirs=""
+for val in $analyzer_dirs; do 
+    rsync_dirs="$rsync_dirs $val/*"
+done
+
+echo "rsync -vu $rsync_dirs $dragonfly_root/$destination"
