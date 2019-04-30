87e42bfc894ff31fe311bcd022a08bfca5787ef4
Andrew Fast <af@counterflowai.com>
2019-04-30 16:39:03 -0400

    Download ip-util files with analyzer-install.sh script

--- analyzer-install.sh
+++ analyzer-install.sh
@@ -42,3 +42,24 @@ done
 # Note: You must copy all config files manually
 echo "install -b --suffix=.old -v $rsync_dirs $dragonfly_root/$destination"
 install -b --suffix=.old -v $rsync_dirs $dragonfly_root/$destination
+
+## For required data files in the ip-util directory
+echo "Downloading helper data sets"
+
+cwd=$(pwd)
+cd /usr/local/dragonfly-mle/analyzer
+
+# For ip-asn.lua
+wget https://iptoasn.com/data/ip2asn-v4-u32.tsv.gz
+gunzip ip2asn-v4-u32.tsv.gz
+
+# For ip-geolocation.lua
+wget https://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.CSV.ZIP
+unzip IP2LOCATION-LITE-DB1.CSV.ZIP
+
+# For ip-blacklist.lua
+wget https://feodotracker.abuse.ch/downloads/ipblocklist.txt
+wget https://ransomwaretracker.abuse.ch/downloads/RW_IPBL.txt
+wget https://zeustracker.abuse.ch/blocklist.php?download=badips -O zeus_badips.txt
+
+cd $cwd
